2336d3ef681e86062a80efbbb85006eb3ae7c28e
pjhhaoshuaia <71443984+pjhhaoshuaia@users.noreply.github.com>
2020-10-27 21:44:25 +0800

    example9-1

--- homework_5/TestCircle.java
+++ homework_5/TestCircle.java
@@ -10,3 +10,21 @@ public class TestCircle {
 		System.out.println("The area of the circle of radius "+circle2.radius+" is "+circle2.getArea());
 	}
 }
+class Circle{
+	double radius;
+	Circle(){
+		radius=1;
+	}
+	Circle(double newRadius){
+		radius=newRadius;
+	}
+	double getArea() {
+		return radius*radius*Math.PI;
+	}
+	double getPerimeter() {
+		return 2*radius*Math.PI;
+	}
+	void setRadius(double newRadius) {
+		radius=newRadius;
+	}
+}
