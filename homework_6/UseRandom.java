d5c3a73217a8446a3f40c20f1caba97e21bc0b07
pjhhaoshuaia <71443984+pjhhaoshuaia@users.noreply.github.com>
2020-11-04 13:55:08 +0800

    9.4

---
+++ homework_6/UseRandom.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class UseRandom {
+	public static void main(String[] args) {
+		Random rand=new Random(1000);
+		for (int i=0;i<50;i++) {
+			System.out.println(rand.nextInt(100));
+		}
+	}
+}
