035a5f6760dd0f6f97f54738515a091b72a1459c
pjhhaoshuaia <71443984+pjhhaoshuaia@users.noreply.github.com>
2020-11-11 15:39:48 +0800

    example11-4

--- homework_7/TestCircleRectangle.java
+++ homework_7/TestCircleRectangle.java
@@ -1,4 +1,4 @@
-public class TestCircleRectangle {
+public class TestCircleRectangle  {
 	public static void main(String[] args) {
 		Circle circle=new Circle(1);
 		System.out.println("A circle "+circle.toString());
